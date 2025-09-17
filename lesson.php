0327bb7a6f047b079b8ec7719ed6f56eb26ccecf
Sinem Şafak <118027044+sinemsafak@users.noreply.github.com>
2025-09-17 18:37:38 +0300

    İlk commit: ilk PHP ders dosyası eklendi

---
+++ lesson.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>PHP | Lesson 1</title>	
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container-fluid">
+
+	<h2><?php echo "my first PHP script"; ?></h2>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>	
+</html>
