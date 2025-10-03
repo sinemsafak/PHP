ca51271ff4744e23e7671b6fefb87ef8a3c4754a
Sinem Şafak <118027044+sinemsafak@users.noreply.github.com>
2025-10-03 19:24:10 +0300

    Çıktı alma komutaları örnekler

---
+++ cikti_alma.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>PHP | Lesson 1</title>	
+<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container-fluid">
+
+	<div class="alert alert-primary mt-5">
+		<?php
+			// echo örnekleri
+			echo "Merhaba Dünya!<br>";
+			echo ("Bu da parantezli echo örneği.<br>");
+			echo "Birden fazla ", "ifade ", "echo ile yazdırılabilir.<br>"; //bu ifade print ile olmaz
+
+			// print örnekleri
+			print "Merhaba PHP!<br>";
+			print ("Bu da parantezli print örneği.<br>"); //<br> ifadesi alt satıra geçmek için kullanılır. string ifadeleri nokta ile birleştiririz
+		?>  
+	</div> 
+</div>
+
+<script src="js/bootstrap.min.js"></script>
+</body>	
+</html>
